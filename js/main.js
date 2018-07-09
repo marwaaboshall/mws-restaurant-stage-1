67e6fc56ecadc625bbb25d33a6c4aa575c3461ca
marwaaboshall <marwa.aboshall@gmail.com>
2018-07-09 15:27:25 +0200

    add tabindex to main page headings

--- js/main.js
+++ js/main.js
@@ -141,10 +141,12 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = "";
   li.append(image);
 
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
+  name.tabIndex = 0;
   li.append(name);
 
   const neighborhood = document.createElement('p');
